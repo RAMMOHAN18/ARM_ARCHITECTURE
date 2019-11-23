fa666fad000d6400eb462ad8d577e2f6b74b2a6a
RAMMOHAN18 <57063255+RAMMOHAN18@users.noreply.github.com>
2019-11-23 17:01:57 +0530

    Add files via upload

---
+++ EXAM/PrintMsg.c
@@ -0,0 +1,21 @@
+#include "stm32f4xx.h"
+#include <string.h>
+#include <stdio.h>
+
+void printMsg(const int a)
+{
+	 float a1= *((float*) &a);
+	 
+	 char Msg1[100];
+	 
+	 char *ptr;
+	 sprintf(Msg1, "%f, \n", a1);
+	 
+	 ptr = Msg1 ;
+   while(*ptr != '\0'){
+      ITM_SendChar(*ptr);
+      ++ptr;
+   
+	 }
+	 ITM_SendChar(',')	 ;
+   }
